39b596c8283ea9969e335813bb4b423eb827213f
ShubhamMhatre2727 <shubhammhatre2727@gmail.com>
2024-06-23 16:23:51 +0530

    added category section

--- src/Category.jsx
+++ src/Category.jsx
@@ -3,7 +3,33 @@ import "./Category.css";
 export default function Category(){
     return(
         <div className="category">
-        gii
+        <h2>Category</h2>
+        <div className="slider">
+            <div className="item">
+                <img src="src\assets\action.webp" alt="" />
+                <h4>Action</h4>
+            </div>
+            <div className="item">
+                <img src="src\assets\adventure.webp" alt="" />
+                <h4>Adventure</h4>
+            </div>
+            <div className="item">
+                <img src="src\assets\drama.webp" alt="" />
+                <h4>Drama</h4>
+            </div>
+            <div className="item">
+                <img src="src\assets\horror.webp" alt="" />
+                <h4>Horror</h4>
+            </div>
+            <div className="item">
+                <img src="src\assets\crime.webp" alt="" />
+                <h4>Crime</h4>
+            </div>
+            <div className="item">
+                <img src="src\assets\animation.webp" alt="" />
+                <h4>Animation</h4>
+            </div>
+        </div>
         </div>
     )
 }
